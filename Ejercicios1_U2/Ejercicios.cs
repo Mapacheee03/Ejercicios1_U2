dc91c9a9c1fe791b1c108780b8adb72ec2bdf7a1
Orlxndo <Orlxndo@DESKTOP-ISPFREG>
2023-10-03 19:40:02 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicios1_U2/Ejercicios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios1_U2
+{
+    internal class Ejercicios
+    {
+
+        public void ImprimirNumeros(int[] numeros)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+        }
+
+        public void CalcularMultiplos(int[] numeros, int multiplo)
+        {
+            int[] resultados = new int[numeros.Length];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                resultados[i] = multiplo * numeros[i];
+            }
+            ImprimirMultiplos(resultados);
+        }
+
+        public void ImprimirMultiplos(int[] numeros)
+        {
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
---
+++ Ejercicios1_U2/Program.cs
@@ -0,0 +1,20 @@
+﻿using EjerciciosPracticos1;
+
+Ejercicios impre = new Ejercicios();
+
+int[] numeros = new int[5];
+for (int X=0;X<numeros.Length;X++) {
+    Console.WriteLine($"Ingrese el Numero {X+1}");
+    numeros[X]=Convert.ToInt32(Console.ReadLine());
+}
+impre.ImPreciones(numeros);
+
+Console.WriteLine("================================");
+Console.WriteLine("INGRESE EL TAMAÑO DEL AREGLO");
+int tamaño = Convert.ToInt32(Console.ReadLine());
+int[] numeros2 = new int[tamaño];
+
+Console.WriteLine("ingrese el multiplo");
+int multiplo = Convert.ToInt32(Console.ReadLine());
+
+impre.Multiplos(numeros2, multiplo);
